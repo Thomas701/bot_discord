984aff0139fe3c81ed43be8a653fb957930740d0
mynoteet <tho.dupois@laposte.net>
2023-10-01 18:20:10 +0200

    bot help command

--- donoteet.py
+++ donoteet.py
@@ -1,10 +1,47 @@
 import discord
+import random
 from discord.ext import commands
+from discord.ext.commands import context
 import aiohttp
+import openai
 
-token
+openai.api_key = "tkt"
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guild_messages = True
+intents.guild_reactions = True
+intents.members = True 
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix="§",description="Beware...")
+bot = commands.Bot(command_prefix="§",description="Beware...", intents=intents)
+bot.remove_command('help')
+
+#-----------------FONCTION EXTERNE------------------------#
+
+def lire_fichier(nom_fichier, encodage='utf-8'):
+    try:
+        with open(nom_fichier, 'r') as fichier:
+            contenu = fichier.read()
+        return contenu
+    except FileNotFoundError:
+        return "Le fichier spécifié n'a pas été trouvé."
+    except Exception as e:
+        return f"Une erreur s'est produite : {str(e)}"
+    
+def correct(text: str):
+    text2 = text.replace('Ã', 'à')
+    text2 = text2.replace('à©', 'é')
+    text2 = text2.replace('à¨', 'è')
+    text2 = text2.replace('àª', 'ê')
+    text2 = text2.replace('à¹', 'ù')
+    text2 = text2.replace('à§', 'ç')
+    return text2
+#---------------------------------------------------------#
+
+#------------------------MUSIC----------------------------#
+
+
+#--------------------------------------------------------#
 
 @bot.event
 async def on_ready():
@@ -14,25 +51,40 @@ async def on_ready():
 async def bonjour(ctx):
     print("Salut.")
     await ctx.send("Salut.")
-    
-@bot.command()
-async def gpt(ctx: commands.Context, *, prompt: str):
-    async with aiohttp.ClientSession() as session:
-        payload = {
-            "model": "text-davinci-003",
-            "prompt": prompt,
-            "temperature": 0.5,
-            "max_tokens": 50,
-            "presence_penalty": 0,
-            "frequency_penalty": 0,
-            "best_of": 1,
-        }
-        headers = {"Authorization": f"Bearer {tkt}"}
-        async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as resp:
-            response = await resp.json()
-            embed = discord.Embed(title="Chat GPT's Response:", description=response["choices"][0]["text"])
-            await ctx.reply(embed=embed)
-    
 
+@bot.command()
+async def help(ctx):
+    texte = lire_fichier("help.txt", encodage='utf-8')
+    await ctx.send(correct(texte)) 
+
+#Fonction random
+@bot.command()
+async def rand(ctx, nbr1 : int , nbr2 : int ):
+    if(nbr2 > 100000000000000000000000000000000):
+        ctx.send("Le nombre entrée est trop long.")
+        return
+    else:
+        if (nbr1 > nbr2):
+            nbre = random.randint(nbr2, nbr1)
+        else:
+            nbre = random.randint(nbr1, nbr2)
+        await ctx.send(f"**Résultat:** {nbre}")
+
+@bot.command()
+async def ask(ctx, *, question):
+    try:
+        # Utilisez l'API GPT-3 pour obtenir une réponse à la question
+        response = openai.Completion.create(
+            engine="davinci",  # Utilisez le moteur GPT-3 de votre choix
+            prompt=question,
+            max_tokens=100,  # Limitez la longueur de la réponse
+            stop=None  # Vous pouvez spécifier des mots pour arrêter la réponse ici
+        )
+
+        # Envoyez la réponse au canal Discord
+        await ctx.send(response.choices[0].text)
+    except Exception as e:
+        await ctx.send(f"Une erreur s'est produite : {str(e)}")
+    
 bot.run("tkt")
 
